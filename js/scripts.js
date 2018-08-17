15cea552fcc7d826142d12dbd36feacedf858838
Mel Yasuda <yasudamel@gmail.com>
2018-08-17 14:40:01 -0700

    add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
 // Business logic
-
 var beepBoop = function(numberInput){
-  // if (isNaN(numberInput) || isNaN(numberInput)) {
-  //   alert("put a number");
-  // } else if (numberInput < 0 || numberInput < 0){
-  //   alert("put a positive number");
-  // }
+  if (isNaN(numberInput) || isNaN(numberInput)) {
+    alert("put a number");
+  } else if (numberInput < 0 || numberInput < 0){
+    alert("put a positive number");
+  }
 
   var numbers = [];
   for (var i = 0; i <= numberInput; i += 1) {
-    numbers.push(i.toString());
+    numbers.push(i.toString(), " ");
    }
    numbers.forEach(function(number){
-    if (number.includes("0")) {
-      numbers.splice(numbers.indexOf(number), 1, 'Beep');
+    if (number.includes("3")) {
+      numbers.splice(numbers.indexOf(number), 1, " I'm sorry, Dave. I'm afraid I can't do that. ");
       } else if (number.includes("1")) {
-      numbers.splice(numbers.indexOf(number), 1, 'Boop');
-    }
+      numbers.splice(numbers.indexOf(number), 1, " Boop ");
+      } else if (number.includes("0")) {
+    numbers.splice(numbers.indexOf(number), 1, " Beep ");
+      }
    });
-   console.log(numbers);
-
+   return numbers;
 }
 
-
-
-// for (var index = numberInput; index >= 1; index -= 1 ) {
-//   result*=index;
-//   }
 
 // User interface
 $(document).ready(function(){
@@ -35,5 +30,6 @@ $(document).ready(function(){
     var numberInput = parseInt($("#number").val());
     var result = beepBoop(numberInput);
       $("#result").append(result);
+
   });
 });
