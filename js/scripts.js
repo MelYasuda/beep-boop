a9db12afad8c31bb7fe158f58c893b4a65d01df3
Mel Yasuda <yasudamel@gmail.com>
2018-08-17 09:58:13 -0700

    add some business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Business logic
 
 var beepBoop = function(numberInput){
+  console.log(typeof numberInput)
   if (isNaN(numberInput) || isNaN(numberInput)) {
     alert("put a number");
   } else if (numberInput < 0 || numberInput < 0){
     alert("put a positive number");
-  }
-  else if (numberInput === 0) {
+  } else if (numberInput.includes("0")) {
     alert("Beep");
   }
 }
@@ -21,7 +21,7 @@ var beepBoop = function(numberInput){
 $(document).ready(function(){
   $("form#numberinput").submit(function(event){
     event.preventDefault();
-    var numberInput = parseInt($("#number").val());
+    var numberInput = $("#number").val();
     var result = beepBoop(numberInput);
       $("#result").append(result);
   });
