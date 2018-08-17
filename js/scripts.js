e81190c03eb9f8675574350fc756d892465547f7
Mel Yasuda <yasudamel@gmail.com>
2018-08-17 14:40:51 -0700

    fix spec for numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,19 @@ var beepBoop = function(numberInput){
   for (var i = 0; i <= numberInput; i += 1) {
     numbers.push(i.toString(), " ");
    }
+
    numbers.forEach(function(number){
-    if (number.includes("3")) {
+     if (number==="0") {
+      numbers.splice(numbers.indexOf(number), 1, " Beep ");
+     } else if (parseInt(number) % 3 === 0) {
       numbers.splice(numbers.indexOf(number), 1, " I'm sorry, Dave. I'm afraid I can't do that. ");
       } else if (number.includes("1")) {
       numbers.splice(numbers.indexOf(number), 1, " Boop ");
       } else if (number.includes("0")) {
-    numbers.splice(numbers.indexOf(number), 1, " Beep ");
+      numbers.splice(numbers.indexOf(number), 1, " Beep ");
       }
    });
+
    return numbers;
 }
 
@@ -27,9 +31,10 @@ var beepBoop = function(numberInput){
 $(document).ready(function(){
   $("form#numberinput").submit(function(event){
     event.preventDefault();
+    result = "";
     var numberInput = parseInt($("#number").val());
     var result = beepBoop(numberInput);
+      $("#result").text(" ");
       $("#result").append(result);
-
   });
 });
